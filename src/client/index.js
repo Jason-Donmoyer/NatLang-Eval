70a3a7146e745099654a74aa5a64d53d5073bf5e
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-10-28 18:32:18 -0400

    sentiment route

--- src/client/index.js
+++ src/client/index.js
@@ -15,3 +15,13 @@ console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+const postData = async (url, data) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+}
--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,8 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+const projectData = {};
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -24,3 +26,47 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+var https = require('follow-redirects').https;
+var fs = require('fs');
+
+var options = {
+    'method': 'POST',
+    'hostname': 'api.meaningcloud.com',
+    'path': `/sentiment-2.1?key=${process.env.API_KEY}&lang=en&of=json&txt=happy%20happy%20every`,
+    'headers': {},
+    'maxRedirects': 20
+};
+
+var req = https.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+        chunks.push(chunk);
+    });
+
+    res.on("end", function (chunk) {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+        console.log(JSON.parse(body));
+        // console.log(body.sentence_list.toString());
+        // console.log(body.toJSON());
+        const data = JSON.parse(body);
+        projectData.data = data;
+    });
+
+    res.on("error", function (error) {
+        console.error(error);
+    });
+});
+
+// app.post('/add', function (req, res) {
+//     res.send(projectData);
+// });
+
+app.get('/sentiment', (req, res) => {
+    res.send(JSON.stringify(projectData));
+    console.log('Get route called');
+});
+
+req.end();
