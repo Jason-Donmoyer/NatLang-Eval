cf8e117cd8dad28cfffe06398b074e527ae99b11
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-11-08 06:06:53 -0500

    installed packages, formHandler function

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,80 @@
+const checkForValidUrl = require('./validityCheck');
+
+const
+
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formInputUrl = document.querySelector('#targetUrl').value;
+    console.log("::: Form Submitted :::");
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    if (checkForValidUrl(formInputUrl)) {
+        console.log('Valid input...')
+
+        fetch('https://localhost:8080/results', {
+                method: 'POST',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'text/plain',
+                },
+                body: formInputUrl,
+            })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+
+
+            });
+    } else {
+        console.log('Not A Valid URL!');
+    }
 }
 
-export { handleSubmit }
+export {
+    handleSubmit
+};
+
+
+
+// async function handleSubmit(event) {
+//     event.preventDefault()
+
+
+
+//     // check what text was put into the form field
+//     let formText = document.getElementById('name').value
+//     //checkForName(formText)
+
+//     let nlpData = await analyzeData('http://localhost:8081/sentiment', formText)
+//         .then(nplData => nlpData.json())
+//         .then((result) => {
+//             console.log(result)
+//         })
+
+
+
+
+
+
+
+//     //console.log(req.body);
+//     // fetch('http://localhost:8080/test')
+//     //     .then(res => res.json())
+//     //     .then(function (res) {
+//     //         document.getElementById('results').innerHTML = res.message
+//     //     })
+// }
+
+// const analyzeData = async function (apiUrl, userUrl) {
+//     let res = await fetch(apiUrl, {
+//         method: 'POST',
+//         credentials: 'same-origin',
+//         headers: {
+//             'Content-Type': 'text/plain'
+//         },
+//         body: userUrl,
+//     })
+
+//     return res
+// }
--- src/server/index.js
+++ src/server/index.js
@@ -1,82 +1,157 @@
+//REQUIRE MODULES
+
+// allow env file access
 const dotenv = require('dotenv');
-dotenv.config();
-var path = require('path')
-const express = require('express')
-var bodyParser = require('body-parser')
-const mockAPIResponse = require('./mockAPI.js')
+dotenv.config;
 
-const projectData = {};
+const path = require('path');
+const fetch = require('node-fetch');
+const mockAPIResponse = require('./mockAPI.js');
+const PORT = 8080;
 
-const app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.static('dist'))
+const bodyParser = require(body - parser);
+app.use(bodyParser.text());
 
-// app.use(bodyParser.urlencoded({
-//     extended: false
-// }))
+const cors = require('cors');
+app.use(cors);
 
-app.use(bodyParser.text())
+// API path and credentials
+const baseApiUrl = 'https://api.meaningcloud.com/sentiment-2.1?key=';
+const ApiKey = process.env.API_KEY;
 
-console.log(__dirname)
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+///////////////////////////////////////////
+app.use(express.static('dist'));
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-    console.log(`Your API key is ${process.env.API_KEY}`);
-})
+// ROUTES
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
-
-var https = require('follow-redirects').https;
-var fs = require('fs');
-
-var options = {
-    'method': 'POST',
-    'hostname': 'api.meaningcloud.com',
-    'path': `/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&url=http://www.twitter.com`,
-    'headers': {},
-    'maxRedirects': 20
-};
-
-var req = https.request(options, function (res) {
-    var chunks = [];
-
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
-    });
-
-    res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-        console.log(JSON.parse(body));
-        // console.log(body.sentence_list.toString());
-        // console.log(body.toJSON());
-        const data = JSON.parse(body);
-        projectData.data = data;
-        console.log([data.status.code, data.status.msg, data.confidence]);
-    });
-
-    res.on("error", function (error) {
-        console.error(error);
-    });
+app.get('/', (req, res) => {
+    res.sendfile('dist/index.html');
 });
 
-// app.post('/', function (req, res) {
-//     res.send(projectData);
-// });
+app.get('/test', (req, res) => {
+    res.send(mockAPIResponse);
+});
 
-app.get('/sentiment', (req, res) => {
-    res.send(JSON.stringify(projectData));
-    console.log('Get route called');
+// POST route and API call
+app.post('/results', async (req, res) => {
+    const response = await fetch(`${baseApiUrl}${ApiKey}&lang=auto&url=${req.body}`);
+
+    try {
+        const data = await response.json();
+        res.send(data);
+    } catch (error) {
+        console.log('There has been an error....', error);
+    }
+});
+
+// SERVER PORT
+app.listen(PORT, () => {
+    console.log(`Server is cocked, locked and ready to rock on port: ${PORT}`);
 });
 
 
 
-req.end();
+
+
+
+// const dotenv = require('dotenv');
+// dotenv.config();
+// var path = require('path')
+// const express = require('express')
+// var bodyParser = require('body-parser')
+// const minipassFetch = require("minipass-fetch")
+// const mockAPIResponse = require('./mockAPI.js')
+
+// const projectData = {};
+
+// const app = express()
+
+// app.use(express.static('dist'))
+
+// // app.use(bodyParser.urlencoded({
+// //     extended: false
+// // }))
+
+// app.use(bodyParser.text())
+
+// console.log(__dirname)
+
+// app.get('/', function (req, res) {
+//     // res.sendFile('dist/index.html')
+//     res.sendFile(path.resolve('src/client/views/index.html'))
+// })
+
+// // designates what port the app will listen to for incoming requests
+// app.listen(8080, function () {
+//     console.log('Example app listening on port 8080!')
+//     console.log(`Your API key is ${process.env.API_KEY}`);
+// })
+
+// // app.get('/test', function (req, res) {
+// //     res.send(mockAPIResponse)
+// // })
+
+// var https = require('follow-redirects').https;
+// var fs = require('fs');
+
+// var options = {
+//     'method': 'POST',
+//     'hostname': 'api.meaningcloud.com',
+//     'path': `/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&url=http://www.twitter.com`,
+//     'headers': {},
+//     'maxRedirects': 20
+// };
+
+// // app.post('/sentiment', function (request, resolve) {
+
+// //     var req = https.request(options, function (res) {
+// //         var chunks = [];
+
+// //         res.on("data", function (chunk) {
+// //             chunks.push(chunk);
+// //         });
+
+// //         res.on("end", function (chunk) {
+// //             var body = Buffer.concat(chunks);
+// //             console.log(body.toString());
+// //             console.log(JSON.parse(body));
+// //             // console.log(body.sentence_list.toString());
+// //             // console.log(body.toJSON());
+// //             const data = JSON.parse(body);
+// //             projectData.data = data;
+// //             console.log([data.status.code, data.status.msg, data.confidence]);
+// //         });
+
+// //         res.on("error", function (error) {
+// //             console.error(error);
+// //         });
+// //     });
+// // })
+
+// app.post('/sentiment', getDataCall)
+
+// async function getDataCall(req, res) {
+//     console.log(req.body.name);
+//     const apiUrl = `/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&url=${req.body.name}`
+//     const response = await minipassFetch(apiUrl)
+//     const recievedData = await response.json()
+//     console.log(recievedData)
+//     res.send(recievedData)
+// }
+
+// // app.post('/', function (req, res) {
+// //     res.send(projectData);
+// // });
+
+// // app.get('/sentiment', (req, res) => {
+// //     res.send(JSON.stringify(projectData));
+// //     console.log('Get route called');
+// // });
+
+
+
+// // req.end();
