59689bb79fee25fd0f974bd025720698ba25a322
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-11-12 17:20:30 -0500

    cleaned up comments

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = 8080;
 
+const serverMsg = [
+    'Server is eagerly waiting on port:',
+    'Server is cocked, locked and ready to rock on port:',
+    'Server is hot to trot on port:',
+    'Server is eavesdropping on port:',
+    'Have you talked to your server today? It\'s listening on port:',
+];
+
+const displayMsg = serverMsg[Math.floor(Math.random() * serverMsg.length)];
+
+
+
 
 //const path = require('path');
 const mockAPIResponse = require('./mockAPI');
@@ -44,176 +56,5 @@ app.post('/results', async (req, res) => {
 
 // Server port
 app.listen(PORT, () => {
-    console.log(`server is hot to trot and ready to rock on port: ${PORT}`);
+    console.log(`${displayMsg} ${PORT}`);
 });
-
-
-
-// //REQUIRE MODULES
-
-// // allow env file access
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-// const path = require('path');
-// const fetch = require('node-fetch');
-// const mockAPIResponse = require('./mockAPI.js');
-// const PORT = 8080;
-
-// const express = require('express');
-// const app = express();
-
-
-
-// const bodyParser = require('body-parser');
-// // app.use(bodyParser.urlencoded({
-// //     extended: false
-// // }));
-// app.use(bodyParser.text());
-
-// const cors = require('cors');
-// app.use(cors);
-
-// // API path and credentials
-// const baseApiUrl = 'https://api.meaningcloud.com/sentiment-2.1?key=';
-// const ApiKey = process.env.API_KEY;
-
-
-// ///////////////////////////////////////////
-// app.use(express.static('dist'));
-
-// // ROUTES
-
-// app.get('/', function (req, res) {
-//     res.sendfile('dist/index.html');
-//     res.sendFile(path.resolve('src/client/views/index.html'))
-// });
-
-// app.get('/test', (req, res) => {
-//     res.send(mockAPIResponse);
-// });
-
-// //POST route and API call
-// // app.post('/results', async (req, res) => {
-// //     console.log(req.body);
-// //     let url = `${baseApiUrl}${ApiKey}&lang=auto&url=${req.body}`;
-// //     console.log(url);
-// //     const response = await fetch(`${baseApiUrl}${ApiKey}&lang=auto&url=http://www.twitter.com`);
-// //     console.log(response);
-
-// //     try {
-// //         const data = await response.json();
-// //         res.send(data);
-// //     } catch (error) {
-// //         console.log('There has been an error....', error);
-// //     }
-// // });
-
-// // SERVER PORT
-// app.listen(8080, () => {
-//     console.log(`Server is cocked, locked and ready to rock on port: ${PORT}`);
-// });
-
-
-
-
-
-
-// // const dotenv = require('dotenv');
-// // dotenv.config();
-// // var path = require('path')
-// // const express = require('express')
-// // var bodyParser = require('body-parser')
-// // const minipassFetch = require("minipass-fetch")
-// // const mockAPIResponse = require('./mockAPI.js')
-
-// //const projectData = {};
-
-// // const app = express()
-
-// // app.use(express.static('dist'))
-
-// // // app.use(bodyParser.urlencoded({
-// // //     extended: false
-// // // }))
-
-// // app.use(bodyParser.text())
-
-// // console.log(__dirname)
-
-// // app.get('/', function (req, res) {
-// //     // res.sendFile('dist/index.html')
-// //     res.sendFile(path.resolve('src/client/views/index.html'))
-// // })
-
-// // // designates what port the app will listen to for incoming requests
-// // app.listen(8080, function () {
-// //     console.log('Example app listening on port 8080!')
-// //     console.log(`Your API key is ${process.env.API_KEY}`);
-// // })
-
-// // // app.get('/test', function (req, res) {
-// // //     res.send(mockAPIResponse)
-// // // })
-
-// // var https = require('follow-redirects').https;
-// // var fs = require('fs');
-
-// // var options = {
-// //     'method': 'POST',
-// //     'hostname': 'api.meaningcloud.com',
-// //     'path': `/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&url=http://www.twitter.com`,
-// //     'headers': {},
-// //     'maxRedirects': 20
-// // };
-
-
-
-// // var req = https.request(options, function (res) {
-// //     var chunks = [];
-
-// //     res.on("data", function (chunk) {
-// //         chunks.push(chunk);
-// //     });
-
-// //     res.on("end", function (chunk) {
-// //         var body = Buffer.concat(chunks);
-// //         console.log(body.toString());
-// //         console.log(JSON.parse(body));
-// //         // console.log(body.sentence_list.toString());
-// //         // console.log(body.toJSON());
-// //         const data = JSON.parse(body);
-// //         projectData.data = data;
-// //         console.log([data.status.code, data.status.msg, data.confidence]);
-// //     });
-
-// //     res.on("error", function (error) {
-// //         console.error(error);
-// //     });
-// // });
-
-
-
-// // app.post('/sentiment', getDataCall)
-
-// // async function getDataCall(req, res) {
-// //     console.log(req.body.name);
-// //     const apiUrl = `/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&url=${req.body.name}`
-// //     const response = await minipassFetch(apiUrl)
-// //     const recievedData = await response.json()
-// //     console.log(recievedData)
-// //     res.send(recievedData)
-// // }
-
-// // // app.post('/', function (req, res) {
-// // //     res.send(projectData);
-// // // });
-
-// // app.post('/results', (req, res) => {
-// //     res.send(JSON.stringify(projectData));
-// //     console.log('Get route called');
-// // });
-
-
-
-// // req.end();
